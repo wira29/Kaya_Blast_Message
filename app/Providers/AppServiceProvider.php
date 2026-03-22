cbacbc22657c6552a8ecd1714cffcee0bb2cdd4e
frizehero <bag4sk4r4@gmail.com>
2026-03-22 18:53:14 +0700

    mod: update bug

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Pagination\Paginator;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,6 +21,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        Paginator::useBootstrapFive();
         if (config('app.env') !== 'local') {
             URL::forceScheme('https');
         }
